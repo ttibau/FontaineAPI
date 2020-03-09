4f65d4aa23a7aa065e1d098cd64d96b4e391175d
Thiago Tibau <ttibau@MacBook-Pro-de-Thiago.local>
2020-03-09 00:11:50 -0300

    Criando método que retorna somente as instalações específicas de um usuário

--- src/app/controllers/installationController.js
+++ src/app/controllers/installationController.js
@@ -1,22 +1,32 @@
 const express = require('express')
 const authMiddleware = require('../middlewares/auth')
 const router = express.Router()
-const Instalation = require('../models/Instalation')
+const Installation = require('../models/Installation')
 
 router.use(authMiddleware)
 
 router.get('/', async (req, res) => {
+    console.log('asdasdkalksd')
     try {
-        const instalations = await Instalation.find().populate('user').populate('product')
+        const instalations = await Installation.find().populate('user').populate('product')
         return res.send({ instalations })
     } catch (error) {
         res.status(400).send({ error: 'Erro ao criar nova instalacao'})
     }
 })
 
+router.get('/myInstallations', async (req, res) => {
+    try {
+        const installations = await Installation.find({ user: req.userId })
+        return res.send({ installations })
+    } catch (error) {
+        res.status(400).send({ error: 'Erro ao buscar minhas instalações'})
+    }
+})
+
 router.get('/:instalationId', async (req, res) => {
     try {
-        const instalation = await Instalation.findById(req.params.instalationId).populate('user').populate('product')
+        const instalation = await Installation.findById(req.params.instalationId).populate('user').populate('product')
         return res.send({ instalation })
     } catch (error) {
         res.status(400).send({ error: 'Erro ao criar nova instalacao'})
@@ -25,7 +35,7 @@ router.get('/:instalationId', async (req, res) => {
 
 router.post('/', async (req, res) => {
     try {
-        const instalation = await Instalation.create({ ...req.body, user: req.userId, product: req.body.productId })
+        const instalation = await Installation.create({ ...req.body, user: req.userId, product: req.body.productId })
         return res.send({ instalation })
 
     } catch (error) {
@@ -36,7 +46,7 @@ router.post('/', async (req, res) => {
 
 router.put('/:instalationId', async (req, res) => {
     try {
-        const instalation = await Instalation.findByIdAndUpdate(req.params.instalationId, 
+        const instalation = await Installation.findByIdAndUpdate(req.params.instalationId, 
             { ...req.body },
             { new: true })
         return res.send({ instalation })
@@ -49,7 +59,7 @@ router.put('/:instalationId', async (req, res) => {
 
 router.delete('/:instalationId', async (req, res) => {
     try {
-        await Instalation.findByIdAndRemove(req.params.instalationId)
+        await Installation.findByIdAndRemove(req.params.instalationId)
         return res.send()
     } catch (error) {
         res.status(400).send({ error: 'Erro ao cexcluir instalacao'})
